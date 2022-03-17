db8d882919974b2e5fd07082a9bc8fa4aaf146a5
igor <igor251999@hotmail.com>
2022-03-17 00:02:15 -0300

    Site Curso Moeda Livre

---
+++ js/js.js
@@ -0,0 +1,26 @@
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+
+  function iframe(){
+   var janela = document.getElementById("janelaexplica");
+    janela.style.display="block";
+  }
